e864bf7d8a4b8eebf9c59fb6d6a8b31311dcedb9
Rafael <76442554+flanderszao@users.noreply.github.com>
2025-06-27 21:24:07 +0000

    pequena correcao

--- parallel/batch-job-mpi.sh
+++ parallel/batch-job-mpi.sh
@@ -15,7 +15,7 @@ mpicc parallel/mandelbrot_mpi.c -o mandelbrot_mpi -lm
 
 for processes in 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32; do
     echo "Running with -np $processes"
-    mpirun np $processes ./mandelbrot_mpi
+    mpirun -np $processes ./mandelbrot_mpi
 done
 
 echo "Job $SLURM_JOB_ID finished at $(date)"
