9feb087202aaf4048b620a5212dc0a512cba5ba1
Rafael <76442554+flanderszao@users.noreply.github.com>
2025-06-27 23:51:50 +0000

    test

--- parallel/batch-job-mpi.sh
+++ parallel/batch-job-mpi.sh
@@ -13,7 +13,7 @@
 # Compila o código MPI
 mpicc parallel/mandelbrot_mpi.c -o mandelbrot_mpi -lm
 
-for processes in 10 12 14 16; do
+for processes in 8; do
     echo "Running with -np $processes"
     mpirun -np $processes ./mandelbrot_mpi
 done
