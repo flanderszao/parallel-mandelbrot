d7541647c4c74fef5877b07c6fe83927c23a2f56
Rafael <76442554+flanderszao@users.noreply.github.com>
2025-06-27 21:19:53 +0000

    batch last

--- parallel/batch-job-mpi.sh
+++ parallel/batch-job-mpi.sh
@@ -13,9 +13,9 @@
 # Compila o código MPI
 mpicc parallel/mandelbrot_mpi.c -o mandelbrot_mpi -lm
 
-for processes in 40 42 44 46 48 50 52 54 56 58 60 62 64; do
+for processes in 2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32; do
     echo "Running with -np $processes"
-    mpirun --oversubscribe -np $processes ./mandelbrot_mpi
+    mpirun np $processes ./mandelbrot_mpi
 done
 
 echo "Job $SLURM_JOB_ID finished at $(date)"
